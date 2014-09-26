ac4c0a860272a5f33ec3f004b0cf8e153b32cde8
ctheien <monsternumber02@gmail.com>
2014-09-26 03:41:07 +0000

    test passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,42 @@
 class UsersController < ApplicationController
     def index
-	@users = User.all
+	    @users = User.all
     end
 
     def new
-	@user = User.new
+	    @user = User.new
     end
 
     def create
-	@user = User.new(params.require(:user).permit(:name, :email, :password))
-	if @user.save
-	    flash[:success] = "Welcome to the site, #{@user.name}"
-	    redirect_to @user
-	else
-	    flash.now[:danger] = "Unable to create new user"
-	    render 'new'
-	end
+      @user = User.new(params.require(:user).permit(:name, :email, :password))
+      if @user.save
+          flash[:success] = "Welcome to the site, #{@user.name}"
+          redirect_to @user
+      else
+          flash.now[:danger] = "Unable to create new user"
+          render 'new'
+      end
     end
 
-    def show
-	@user = User.find(params[:id])
-    rescue
-	flash[:danger] = "Unable to find user"
-	redirect_to users_path
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:name, :email, :password))
+      flash[:success] = "Updated, #{@user.name}"
+      redirect_to @user
+    else
+      flash.now[:danger] = "Unable to modify your account"
+      render 'edit'
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+      rescue
+    flash[:danger] = "Unable to find user"
+    redirect_to users_path
+  end
 end
